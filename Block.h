37cefaf1b385dfc35c8ea5f9c0c9458d65364ff9
ymhrmnk <ymhr_mnk@yahoo.co.jp>
2013-04-21 10:24:02 +0900

    Update Block.h

--- Block.h
+++ Block.h
@@ -1,24 +1,11 @@
 #ifndef _BLOCK_
 #define _BLOCK_
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 
-#include "globaldefs.h"
-#ifndef MIFFY_FRUSTUM
-#include <miffy/math/collisiondetection/frustum.h>
+#ifndef MIFFY_VEC2
+#include <miffy/math/vec2.h>
 #endif
-
-
-#ifndef _CG_
-#include "Cg.h"
-#endif
-
-class Cg;
-
-#ifndef _FILE_
-#include "File.h"
-#endif
-
-class File;
-
 #ifndef MIFFY_VEC3
 #include <miffy/math/vec3.h>
 #endif
@@ -28,8 +15,24 @@ class File;
 #ifndef MIFFY_MATRIX
 #include <miffy/math/matrix.h>
 #endif
+#ifndef MIFFY_FRUSTUM
+#include <miffy/math/collisiondetection/frustum.h>
+#endif
+#include <miffy/math/colorpalette.h>
 
-//class Matrix3x3;
+
+#include "globaldefs.h"
+#ifndef _CG_
+#include "Cg.h"
+#endif
+class Cg;
+#ifndef _FILE_
+#include "File.h"
+#endif
+class File;
+
+
+
 using namespace miffy;
 
 class Block 
@@ -37,11 +40,11 @@ class Block
 
 
 public:
-  int x;//x方向のブロックインデックス（level0だと0～32の値、level4だと0か1）
+	int x;//x方向のブロックインデックス（level0だと0～32の値、level4だと0か1）
 	int y;//y方向のブロックインデックス（level0だと0～32の値、level4だと0か1）
 	int z;//z方向のブロックインデックス（level0だと0～16の値、level4だと0）
 	short int level;
-	int bnumx;//x方向のブロックの個数（levelによって一意に決まる）
+	int bnumx;///< x方向のブロックの個数（levelによって一意に決まる）
 	int bnumy;
 	int bnumz;
 
@@ -53,7 +56,7 @@ public:
 	static unsigned int* rRequestBlock[NUMLEVEL];
 	//static bool**** occludedIndex;//[NUMLEVEL][32][32][16];
 
-
+	//比率をそろえるための何かよね。
     static float brX,brY,brZ;
 	static float iniX,iniY,iniZ;
 	static bool resocolorflag;
@@ -72,7 +75,8 @@ public:
 	//9occlusionculling　オクルージョンカリングされたもの（renderBlock()で使われる）
 
 	static float  CLR[NUMLEVEL][3];
-	
+	bool isSameBlock(Block testBlock);
+	int IsInFrustum(const frustum<double>& _frustum);
 	Block(int x,int y,int z,short int level,int bnumx,int bnumy,int bnumz);
 	Block(void);
 	~Block();
@@ -80,7 +84,7 @@ public:
 	void setBlock(int x,int y,int z,short int level,int bnumx,int bnumy,int bnumz);
 	static void renderSubblock(unsigned int** rblock,Block bl,float alpha);
 
-	bool  resoChange(double* modelmatrix,double* projmatrix,int winwidth,int winheight);
+	bool  resoChange(const mat4<double>& _modelmatrix,const mat4<double>& _projmatrix,const vec2<int>& _winsize,int _levelLimit);
 	float calcDistanceFromcamera(double* modelmatrix);
 
 	void RayCast(Cg* Cg,int dif);
@@ -92,7 +96,7 @@ public:
 	Block getHighblock();
 	static Block GetRootBlock();
 	void setBlockState(blockState state,unsigned int** renderblock);
-	vec3<float> getBlockVec(const mat4<float>& _modelViewMatrix);
+	vec3<double> getBlockVec(const mat4<double>& _modelViewMatrix);
 	bool testOcclusion(Cg* Cg);//もしアルファ値がthresholdよりも小さいフラグメントが存在するならtrue,存在しないならflaseが変える
 	blockState getLowState(Block lowblock);
 	void earlyRayTermination(Cg* Cg);
